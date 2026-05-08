7cdca9f91c453844989467b8050acc6d3f62ea77
David Dadzie <ddadzie124@gmail.com>
2026-05-08 04:29:07 +0000

    deleted:    admin/assets/js/users.js 	modified:   admin/dashboard.php 	deleted:    admin/users.php 	modified:   backend/api/admin/auth.php 	deleted:    backend/api/admin/users.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,12 +48,6 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
                     </svg>
                     Add Project
                 </a></li>
-                <li><a href="/admin/users.php">
-                    <svg fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
-                    </svg>
-                    Manage Users
-                </a></li>
                 <li><a href="/admin/login.php?logout=1">
                     <svg fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"/>
